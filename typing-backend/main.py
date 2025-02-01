ad5c36ee85d6960e3f189baac6b3f18b57d10800
Ashish Jain <ashishjain99@yahoo.com>
2025-02-01 11:25:58 -0700

    Added new create route for posting data

--- typing-backend/main.py
+++ typing-backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.user import Router as User
+from routes.create import Router as Create
 
 server = FastAPI()
 
 server.include_router(User, prefix="/api")
+server.include_router(Create, prefix="/api")
 
 @server.get("/")
 def Root():
