fa662f1c94cc51bb6c7e6cc22353b4383737a853
The-Supreme-Elemental <jamboyh5@gmail.com>
2025-02-08 13:41:43 -0700

    removed excess text

--- typing-champion/src/components/Header.vue
+++ typing-champion/src/components/Header.vue
@@ -2,15 +2,13 @@
 </script>
 
 <template>
-<<<<<<< HEAD
-    <header>
+    <div>
         <h1>Home</h1>
         <nav>
             <li><router-link to="/">Home</router-link></li>
             <li><router-link to="/about">About</router-link></li>
             <li><router-link to="/contact">Contact</router-link></li>
         </nav>  
-    </header>
+    </div>
     
-=======
 </template>
