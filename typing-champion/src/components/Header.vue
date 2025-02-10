24336452cda00a7d6c40b03f903c8f2a43dc6be5
The-Supreme-Elemental <jamboyh5@gmail.com>
2025-02-10 00:50:52 -0700

    Styled header

--- typing-champion/src/components/Header.vue
+++ typing-champion/src/components/Header.vue
@@ -2,13 +2,42 @@
 </script>
 
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="header">
         <nav>
-            <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="/about">About</router-link></li>
-            <li><router-link to="/contact">Contact</router-link></li>
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/about">About</router-link></li>
+                <li><router-link to="/contact">Contact</router-link></li>
+            </ul>
         </nav>  
     </div>
     
 </template>
+
+<style scoped>
+    nav {
+        display: flex;
+        margin: 0 auto;
+        /* padding-right: 100px; */
+        /* ^^^^^ This pushes the nav over to the center */
+    }
+
+    nav ul {
+        display: flex;
+        margin: 0 0 0 auto;
+        /* list-style-type: none; */
+        /* width: 100%; */
+        /* padding: 0; */
+    }
+
+    nav li {
+        display: flex;
+        align-items: center;
+        padding: 0 20px;
+        height: 100%;
+        transition: 0.75s;
+    }
+
+
+
+</style>
