dfe73a4b3958e1845d80eda0e2870a68a5d526bb
The-Supreme-Elemental <jamboyh5@gmail.com>
2025-02-12 12:03:36 -0700

    idk what i did lol

--- typing-champion/src/App.vue
+++ typing-champion/src/App.vue
@@ -5,6 +5,13 @@ import { RouterView } from 'vue-router';
 </script>
     
 <template>
+    <!-- <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav> -->
     <Header />
     <RouterView />
     <Footer />
